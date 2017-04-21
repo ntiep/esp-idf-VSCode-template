6b33d6913eb68e3a6269f7e1f34256dbb0d9b0d1
FHFS <frank.schrijver@gmail.com>
2017-04-21 17:42:21 +0200

    renamed and fixed paths

--- main/main.c
+++ main/main.c
@@ -15,28 +15,26 @@ void app_main(void)
 {
     nvs_flash_init();
     tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     wifi_config_t sta_config = {
         .sta = {
-            .ssid = "access_point_name",
+            .ssid = "fhfs",
             .password = "password",
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
+            .bssid_set = false}};
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_12, GPIO_MODE_OUTPUT);
     int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
+    while (true)
+    {
+        gpio_set_level(GPIO_NUM_12, level);
         level = !level;
         vTaskDelay(300 / portTICK_PERIOD_MS);
     }
 }
-
